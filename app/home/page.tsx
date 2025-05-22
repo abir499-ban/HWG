2b618514ce8d00dfcbc6057d2d130f308a1fe8ab
Abir Banerjee <abirbanerjee793@gmail.com>
2025-05-22 20:38:54 +0530

    ui fixes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,25 @@
 'use client'
 import {useSession} from 'next-auth/react'
+import React , { useEffect } from 'react'
+import {useRouter} from 'next/navigation'
+import {Loader} from 'lucide-react'
 
 
 export default function Homr(){
+    const router = useRouter()
    const {data , status} = useSession()
+
+   useEffect(()=>{
+    if(status === 'unauthenticated'){
+        console.log('unauthenticated, log in to view this page')
+        router.push('/auth/signin')
+    }
+   }, [status])
+   
+    if(status === 'loading'){
+        return <Loader className='size-56'/>
+    }
+
     return(
         <div>
             <h1>This is a farmer's Dashboard</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import Link from 'next/link'
-
+import React from 'react';
+import Link from 'next/link';
+import './globals.css';
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4 text-green-700">Welcome to the AgriFund</h1>
-        <p className="text-xl text-gray-700 mb-8">
+    <div
+      className="relative min-h-screen bg-cover bg-no-repeat bg-[url('/assets/catch-crop-4133941.jpg')] flex flex-col items-center justify-center"
+    >
+      <div className="relative z-10 text-center px-6 md:px-4">
+        <h1 className="text-4xl md:text-8xl font-bold mb-4 text-white drop-shadow-lg">
+          Welcome to the AgriFund
+        </h1>
+        {/* <p className="text-xl md:text-2xl font-mono font-bold text-[#f0cbb6] drop-shadow-lg mb-6 text-stroke">
           Funding the agriculture of Gujarat.
-        </p>
-        <Link
-          href="/auth/signup"
-          className="inline-flex items-center px-6 py-3 rounded-lg shadow-md bg-green-700 text-white font-semibold hover:bg-green-800 transition"
-        >
-          Farmer Sign Up
-        </Link>
+        </p> */}
+        <div className='flex flex-col gap-2 md:gap-4 items-center justify-center'>
+          <Link
+            href="/auth/signup"
+            className="inline-flex items-center px-6 py-3 rounded-lg shadow-md bg-green-700 text-white font-semibold hover:bg-green-800 transition duration-300"
+          >
+            Farmer Sign Up
+          </Link>
+          <Link
+            href="/auth/signin"
+            className="inline-flex items-center px-6 py-3 rounded-lg shadow-md bg-green-700 text-white font-semibold hover:bg-green-800 transition duration-300"
+          >
+            Farmer Sign In
+          </Link>
+        </div>
       </div>
     </div>
   );
